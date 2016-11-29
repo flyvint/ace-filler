57c78df965fb2875bb6102092a1bf720d4e48c1e
vint <flyvint@gmail.com>
2016-11-29 17:14:42 +0300

    done gui frontend; some fixes in error handling

--- ace-filler.sh
+++ ace-filler.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
+cleverfile=$( kdialog --title "Файл заказа Clever" --getopenfilename $HOME "application/vnd.ms-excel application/vnd.oasis.opendocument.spreadsheet" )
+[[ $? != "0" ]] && exit 1
 
-kdialog --title "Файл заказа Clever" --getopenfilename :label1 "application/vnd.ms-excel application/vnd.oasis.opendocument.spreadsheet"
+smfile=$( kdialog --title "Файл заказа Супермамок" --getopenfilename "$( dirname $cleverfile )" "text/csv" )
+[[ $? != "0" ]] && exit 1
+
+echo "$cleverfile"
+echo "$smfile"
+
+kdialog --msgbox "Делаю заказ..." &
+msgpid=$!
+
+tmplog="/tmp/ace-filler.$$.log"
+outfile="${cleverfile%.xls}.out.xls"
+
+./ace-filler "$cleverfile" "$smfile" "$outfile" 2>&1 | tee $tmplog
+retv=${PIPESTATUS[0]}
+kill $msgpid
+
+if [ $retv -eq 0 ]; then
+    kdialog --msgbox "Заказ сохранен в файле $outfile"
+    grep "!!!" $tmplog > ${tmplog}2
+    kdialog --textbox ${tmplog}2 600 400
+else
+    kdialog --msgbox "Не получилось сделать заказ :("
+    kdialog --textbox ${tmplog} 600 400
+fi
